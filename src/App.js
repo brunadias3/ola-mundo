9addf81aff7ca24d020bdb27e7f62d63fc2d2e59
Bruna <brunadias_30@hotmail.com>
2023-01-23 19:07:24 -0300

    criando rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Inicio from './paginas/Inicio'
+import SobreMim from './paginas/SobreMim'
+
 
 function App() {
   return (
-    <div>
-      <h1>Olá Mundo!</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path='/sobremim' element={<SobreMim />} />
+        <Route path='*' element={<div>Pagina nao encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
