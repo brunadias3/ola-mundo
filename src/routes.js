124c7fd18406605cc221b8e4569868c9c9b31dfc
Bruna <brunadias_30@hotmail.com>
2023-01-23 22:38:15 -0300

    post e react markdown

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import SobreMim from './paginas/SobreMim'
 import Menu from './componentes/Menu'
 import Rodape from './componentes/Rodape';
 import PaginaPadrao from './componentes/PaginaPadrao';
+import Post from './paginas/Post';
 
 
 function AppRoutes() {
@@ -11,9 +12,10 @@ function AppRoutes() {
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path='/' element={<PaginaPadrao />}>
+        <Route path='/' element={<PaginaPadrao />} >
           <Route index element={<Inicio />} />
           <Route path='sobremim' element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
         <Route path='*' element={<div>Pagina nao encontrada</div>} />
       </Routes>
