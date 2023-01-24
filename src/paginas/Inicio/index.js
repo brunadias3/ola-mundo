9b22bf7d5c0a2476210a10bf1bab9b204008cd3e
Bruna <brunadias_30@hotmail.com>
2023-01-23 21:51:20 -0300

    pagina padrao

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Banner from '../../componentes/Banner'
 import styles from './Inicio.module.css'
 import posts from '../../json/posts.json'
 import Post from '../../componentes/Post'
@@ -7,17 +6,13 @@ import Post from '../../componentes/Post'
 
 export default function Inicio() {
   return (
-    <main>
-      <Banner />
+    <ul className={styles.posts}>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Post post={post} />
+        </li>
+      ))}
+    </ul>
 
-      <ul className={styles.posts}>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Post post={post} />
-          </li>
-        ))}
-      </ul>
-    </main>
-    
   )
 }
--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Banner from '../../componentes/Banner'
 
 export default function SobreMim() {
   return (
+
     <h1>Sobre mim...</h1>
+
   )
 }
