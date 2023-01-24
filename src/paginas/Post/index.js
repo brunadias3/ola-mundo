a1a9b36fd37a1073693f69168e60490cca4c07b9
Bruna <brunadias_30@hotmail.com>
2023-01-23 23:44:55 -0300

    arrumando rotas

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,9 +1,10 @@
 import './Post.css'
-import { useParams } from 'react-router-dom'
+import { Route, Routes, useParams } from 'react-router-dom'
 import posts from '../../json/posts.json'
 import PostModelo from '../../componentes/PostModelo'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import NaoEncontrada from '../NaoEncontrada'
+import PaginaPadrao from '../../componentes/PaginaPadrao'
 
 export default function Post() {
 
@@ -13,21 +14,28 @@ export default function Post() {
         return post.id === Number(parametros.id)
     })
 
-    if(!post) {
+    if (!post) {
         return (
             <NaoEncontrada />
         )
     }
 
     return (
-        <PostModelo
-            fotoCapa={`../../assets/posts/${post.id}/capa.png`}
-            titulo={post.titulo}>
-            <div className='post-markdown-container'>
-                <ReactMarkdown>
-                    {post.texto}
-                </ReactMarkdown>
-            </div>
-        </PostModelo>
+        <Routes>
+            <Route path='*' element={<PaginaPadrao />}>
+                <Route index element={
+                    <PostModelo
+                        fotoCapa={`../../assets/posts/${post.id}/capa.png`}
+                        titulo={post.titulo}
+                    >
+                        <div className='post-markdown-container'>
+                            <ReactMarkdown>
+                                {post.texto}
+                            </ReactMarkdown>
+                        </div>
+                    </PostModelo>
+                } />
+            </Route>
+        </Routes>
     )
 }
